63cbcb2b2cc96726cbe7a2ac9d6edb7552e80a53
spygg <spygg@spygg.DHCP>
2017-03-30 22:33:27 +0800

    修改setting建立base模板

--- liusblog/settings.py
+++ liusblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1#88m0ntpj@u7u3lup_gj#+cul^%csw9=)#*y#vre5zd2)+***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
